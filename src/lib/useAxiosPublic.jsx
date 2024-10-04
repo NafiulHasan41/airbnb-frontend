846eef28e366b03fcc0f3907f6ab532522b19250
NafiulHasan41 <nhashik40@gmail.com>
2024-10-05 05:40:28 +0600

    Deployed the site

--- src/lib/useAxiosPublic.jsx
+++ src/lib/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://airbnb-server-mauve.vercel.app'
 })
 
 const useAxiosPublic = () => {
