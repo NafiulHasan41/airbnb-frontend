cbd8f64f4fee212416f2e1b6fcd469d1e2980257
NafiulHasan41 <nhashik40@gmail.com>
2024-10-05 04:24:42 +0600

    tried to refurnish some code

--- src/components/Listings_show.jsx
+++ src/components/Listings_show.jsx
@@ -2,13 +2,14 @@
 
 import { useSelector } from "react-redux";
 import Card_airbnb from "./Card_airbnb";
+import Loading from "./Loading";
 
 const Listings_show = () => {
     const listings = useSelector((state) => state.listings.listings);
     const loading = useSelector((state) => state.listings.loading);
 
-    if (loading) return <p>Loading...</p>;
-    if (!listings || listings.length === 0) return <p>No listings available</p>;
+    if (loading) return <Loading/>;
+    if (!listings || listings.length === 0) return <p className=" text-rose-500 text-center mt-20 text-2xl font-bold w-[50%] mx-auto">No listings available</p>;
   
     return (
        <>
