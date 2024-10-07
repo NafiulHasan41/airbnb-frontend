ace6eea1902dfe7ea9e81712b219014f56452852
NafiulHasan41 <nhashik40@gmail.com>
2024-10-08 03:51:42 +0600

    Solved some bug

--- src/components/BookCard/Book_card.jsx
+++ src/components/BookCard/Book_card.jsx
@@ -6,16 +6,17 @@ import Image from 'next/image';
 const Book_card = ( { host}) => {
     const img = host?.avatarUrl ? host.avatarUrl : 'https://png.pngtree.com/png-vector/20240611/ourmid/pngtree-flat-people-icon-illustration-vector-png-image_12650659.png';
     // console.log(img);
+    // change some part
     return (
         <div className="book-container group">
         <div className="book  ">
-           <div className="flex items-center justify-center  ">
+           <div className="flex  items-center justify-center  ">
            <Image
             src={img}
             width={45}
             height={45}
             alt="book"
-            className="  rounded-full border border-black "
+            className="w-12 h-12  rounded-full border border-black "
           />
            </div>  
         </div>
